9ed67e5014db9d2c6e63d668cf88c87b5b836547
Chiamaka Osumgba <osumgbachiamaka@gmail.com>
2020-08-08 16:48:08 +0100

    create server with express

---
+++ routes/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const indexRoute = require('../routes/index')
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use('/', indexRoute);
+
+module.exports = app;
